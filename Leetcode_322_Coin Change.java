a1549261f587e4c938831fff14988aaffdd9a313
Mengli Yuan <ymenglifall@gmail.com>
2020-10-20 21:56:12 -0400

    test

---
+++ Leetcode_322_Coin Change.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class leetcode_322 {
+public static void main(String[] args) {
+       int[] coins = {1,2,5};
+       int amount = 11;
+       int reuslt = coinChange(coins, amount);
+       System.out.println("result =" +reuslt);
+
+   
+   }
+
+
+   public static int coinChange(int[] coins, int amount) {
+    int[] dp = new int[amount+1];
+    Arrays.fill(dp, amount+1);
+    dp[0] = 0;
+    for(int i=0; i<= amount; i++){
+        for(int j=0; j<coins.length; j++){
+            if(coins[j] <= i){
+                dp[i] = Math.min(dp[i], 1+dp[i-coins[j]]);
+            }
+        }
+    }
+    return dp[amount] > amount? -1:dp[amount];
+}
+}

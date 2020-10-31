b40ae20e94d85dec45e6792b103303b904812c6e
Mengli Yuan <ymenglifall@gmail.com>
2020-10-30 23:48:55 -0400

    add comments

--- Leetcode_877_Stone Game.java
+++ Leetcode_877_Stone Game.java
@@ -3,7 +3,14 @@ class Solution {
         int [][] memo = new int[piles.length][piles.length];
         return helper(piles, 0, piles.length-1, memo) >= 0;
     }
-    
+    /**
+     * Get max score diff from myScore and other player Score
+     * @param piles
+     * @param l
+     * @param r
+     * @param memo
+     * @return
+     */
     private int helper(int[]piles, int l, int r, int[][]memo){
         if(l == r){
             return piles[l];
@@ -11,6 +18,8 @@ class Solution {
         if(memo[l][r] != 0){
             return memo[l][r];
         }
+        /**There are two option for me, if I pick left side pile, score diff would be pile[l] - other player score */
+        /** If I pick right side pile, score diff would be pile[r] - other player score */
         int r1 = piles[l] - helper(piles, l+1, r, memo);
         int r2 = piles[r] - helper(piles, l, r-1, memo);
         memo[l][r] = Math.max(r1, r2);

115b998315253ae5bad97e94fe18d176d3356ecf
Mengli Yuan <ymenglifall@gmail.com>
2020-12-19 22:55:50 -0500

    initial commit

---
+++ 1106. Parsing A Boolean Expression.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean parseBoolExpr(String expression) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i = 0; i< expression.length(); i++){
+            char c = expression.charAt(i);
+            if(c == ')'){
+                Set<Character> seen = new HashSet<>();
+                while(stack.peek() != '('){
+                    seen.add(stack.pop());
+                }
+                stack.pop();        // pop out '('
+                char operator = stack.pop();
+                if(operator == '&') {
+                    stack.push(seen.contains('f')? 'f':'t');
+                }
+                else if(operator == '|'){
+                    stack.push(seen.contains('t')? 't':'f');
+                }
+                else {
+                    stack.push(seen.contains('t')? 'f':'t');
+                }                  
+            }
+            else if(c != ','){
+                stack.push(c);
+            }
+        }
+        return stack.peek() == 't';
+    }
+}

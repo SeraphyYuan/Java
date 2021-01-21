f6e8529adde6e8c16930d45049bdc36224b96a32
Mengli <ymenglifall@gmail.com>
2021-01-21 14:23:09 -0500

    second time

--- 1306. Jump Game III.java
+++ 1306. Jump Game III.java
@@ -25,3 +25,31 @@ class Solution {
          
     }
 }
+
+//Second Time code
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        Deque<Integer> q = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        q.add(start);
+        visited.add(start);
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            if(arr[curr] == 0){
+                return true;
+            }
+            int left = curr - arr[curr];
+            int right = curr + arr[curr];
+            int len = arr.length;
+            if(left >= 0 && left < len && !visited.contains(left))  {
+                q.add(left);
+                visited.add(left);
+            }
+            if(right >= 0 && right < len && !visited.contains(right))  {
+                q.add(right);
+                visited.add(right);
+            }
+        }
+        return false;
+    }
+}
